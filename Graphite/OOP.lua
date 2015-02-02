ee2cacda2283531425ddf26db6f6a7b63f55756a
Lucien Greathouse <me@lpghatguy.com>
2015-02-02 13:42:53 -0700

    Pooled attributes, internal restructuring. Custom attributes are probably broken.

--- Graphite/OOP.lua
+++ Graphite/OOP.lua
@@ -44,74 +44,127 @@ config:_lock()
 
 -- Utility methods
 local function handle_indirection(class, instance)
-	if (class.attributes.InstanceIndirection) then
+	if (class.__attributes.InstanceIndirection) then
 		local internal = instance
 		instance = newproxy(true)
 
 		local meta = getmetatable(instance)
-		meta.__index = internal
-		meta.__newindex = internal
-		meta.__pairs = internal
-		meta.__ipairs = internal
-		meta.__gc = function(self)
-			if (self[name_destructor]) then
-				return self[name_destructor](self)
-			end
-		end
+		meta.__index = meta.__index or internal
+		meta.__newindex = meta.__newindex or internal
+		meta.__pairs = meta.__pairs or internal
+		meta.__ipairs = meta.__ipairs or internal
 	end
 
 	return instance
 end
 
 local function apply_metatable(class, instance)
-	if (class.attributes.InstanceIndirection) then
+if (class.__attributes.InstanceIndirection) then
 		-- If we wrapped the object in a userdata, we need to apply metatables a little differently.
-		Dictionary.ShallowCopy(class.metatable, getmetatable(instance))
+		Dictionary.ShallowCopy(class.__metatable, getmetatable(instance))
 	else
 		-- The InstancedMetatable attribute determines whether the metatable
 		-- is class-specific or instance-specific.
-		if (class.attributes.InstancedMetatable) then
-			setmetatable(instance, Dictionary.ShallowCopy(class.metatable))
+		if (class.__attributes.InstancedMetatable) then
+			setmetatable(instance, Dictionary.ShallowCopy(class.__metatable))
 		else
-			setmetatable(instance, class.metatable)
+			setmetatable(instance, class.__metatable)
 		end
 	end
 end
 
 OOP.Attributes = {
 	Class = {},
-	PreConstructor = {},
-	PostConstructor = {},
+	PreInitialize = {},
+	PostInitialize = {},
 	Copy = {}
 }
 
-function OOP:RegisterAttribute(type, name, application)
+--[[
+	void OOP:RegisterAttribute(string type, string name, function method)
+		type: The type of attribute (Class, PreInitialize, PostInitialize, or Copy).
+		name: The name of the attribute as a class would call it.
+		method: The class applicator. For function signatures, see below.
+
+	Registers a new custom class attribute.
+
+	Class:
+		void function(class)
+
+		Called immediately when the attribute is added to the class.
+
+	PreInitialize:
+		void function(class, instance)
+
+		Called after instance allocation but before the object's initializer is called.
+
+	PostInitialize:
+		void function(class, instance)
+
+		Called after the object is fully initialized.
+
+	Copy:
+		void function(original, copy)
+
+		Called after the copied object has been allocated and filled.
+]]
+function OOP:RegisterAttribute(type, name, method)
 	local typeset = self.Attributes[type]
 
 	if (not typeset) then
 		error(("Could not register attribute of type %q: unknown attribute type"):format(type), 2)
 	end
 
-	typeset[name] = application
+	typeset[name] = method
 end
 
+-- PooledInstantiation attribute
+-- The rest of the functionality is defined inline, since it modifies allocation behavior.
+OOP.Attributes.Class["PooledInstantiation"] = function(class)
+	class.__pool = {}
+	class.__pool_destructor = function(instance)
+		local pool = class.__pool
+		if (#pool < (class.__attributes.PoolSize or 20)) then
+			table.insert(pool, instance)
+		end
+	end
+
+	class.__members[name_destructor] = class.__members[name_destructor] or class.__pool_destructor
+	class.__metatable.__gc = class.__metatable.__gc or class.__pool_destructor
+end
+
+--[[
+	Body for both Class and StaticClass base classes.
+
+	__members: Holds user-defined class members.
+	__base_members: Holds base class members so they can be overridden effectively.
+	__metatable: Holds metatable to be applied to instances.
+	__attributes: Holds class attributes.
+	__typecheck: The typecheck object for this class.
+]]
 OOP.BaseClass = {
-	members = {},
-	metatable = {},
-	attributes = {},
-	typecheck = {}
+	__members = {},
+	__base_members = {},
+	__metatable = {},
+	__attributes = {},
+	__typecheck = {}
 }
 
+--[[
+	Class BaseClass:Inherits(...)
+
+	Inherits from classes, taking on their attributes, members, metatables, and type information.
+]]
 function OOP.BaseClass:Inherits(...)
 	for i = 1, select("#", ...) do
 		local object = select(i, ...)
 
-		Dictionary.DeepCopyMerge(object.members, self.members)
-		Dictionary.DeepCopyMerge(object.metatable, self.metatable)
-		Dictionary.ShallowMerge(object.attributes, self.attributes)
-		Dictionary.ShallowMerge(object.typecheck, self.typecheck)
+		Dictionary.DeepCopyMerge(object.__members, self.__members)
+		Dictionary.DeepCopyMerge(object.__metatable, self.__metatable)
+		Dictionary.ShallowMerge(object.__attributes, self.__attributes)
+		Dictionary.ShallowMerge(object.__typecheck, self.__typecheck)
 
-		for attribute in pairs(object.attributes) do
+		for attribute in pairs(object.__attributes) do
 			local found = OOP.Attributes.Class[attribute]
 			if (found) then
 				found(self)
@@ -122,8 +175,15 @@ function OOP.BaseClass:Inherits(...)
 	return self
 end
 
+--[[
+	Class BaseClass:Attributes(table attributes)
+		attributes: The attributes to give to the object.
+
+	Adds attributes to the class. Overwrites existing attributes.
+	The attributes parameter is only shallow copied, keep this in mind.
+]]
 function OOP.BaseClass:Attributes(attributes)
-	Dictionary.ShallowCopy(attributes, self.attributes)
+	Dictionary.ShallowCopy(attributes, self.__attributes)
 
 	for attribute in pairs(attributes) do
 		local found = OOP.Attributes.Class[attribute]
@@ -135,34 +195,64 @@ function OOP.BaseClass:Attributes(attributes)
 	return self
 end
 
+--[[
+	Class BaseClass:Metatable(table metatable)
+		metatable: The metatable to give to instances of this class.
+
+	Adds metatables to the class. Overwrites existing metatable entries.
+	The metatable parameter is only shallow copied, keep this in mind.
+]]
 function OOP.BaseClass:Metatable(metatable)
-	Dictionary.ShallowCopy(metatable, self.metatable)
+	Dictionary.ShallowCopy(metatable, self.__metatable)
 
 	return self
 end
 
+--[[
+	Class BaseClass:Metatable(table member)
+		member: The member to give to instances of this class.
+
+	Adds members to the class. Overwrites existing member entries.
+	The members parameter is only shallow copied, keep this in mind.
+]]
 function OOP.BaseClass:Members(members)
-	Dictionary.ShallowCopy(members, self.members)
+	Dictionary.ShallowCopy(members, self.__members)
 
 	return self
 end
 
+-- The base object for all non-singleton objects
 OOP.Object = Dictionary.DeepCopy(OOP.BaseClass)
-	--:Attributes(default_attributes)
+	:Attributes(default_attributes)
 
-OOP.Object.members.class = newproxy(true)
-getmetatable(OOP.Object.members.class).__index = OOP.Object
+OOP.Object.__members.class = newproxy(true)
+getmetatable(OOP.Object.__members.class).__index = OOP.Object
 
-OOP.Object.typecheck[OOP.Object] = true
+OOP.Object.__typecheck[OOP.Object] = true
 OOP.Object[name_type_checker] = OOP.Object.typecheck
 
-OOP.Object[name_placement_constructor] = function(self, instance, ...)
-	local instance = {}
+--[[
+	[This method can be renamed, see Graphite/_.lua]
+	Object Class:PlacementNew(indexable target?, ...)
+		target: Where to place the instance, will be provided if not given.
 
-	if (self.attributes.SparseInstances) then
-		setmetatable(instance, {__index = self.members})
+	Creates a new object and puts it into a given indexable object.
+]]
+OOP.Object[name_placement_constructor] = function(self, instance, ...)
+	if (not instance) then
+		if (self.__attributes.PooledInstantiation) then
+			instance = table.remove(self.__pool, #self.__pool) or {}
+		else
+			instance = {}
+		end
+	end
+
+	if (self.__attributes.SparseInstances) then
+		Dictionary.DeepCopy(self.__base_members, instance)
+		setmetatable(instance, {__index = self.__members})
 	else
-		Dictionary.DeepCopy(self.members, instance)
+		Dictionary.DeepCopy(self.__base_members, instance)
+		Dictionary.DeepCopy(self.__members, instance)
 	end
 
 	instance.self = instance.self or instance
@@ -172,7 +262,7 @@ OOP.Object[name_placement_constructor] = function(self, instance, ...)
 
 	-- We wrap the typechecking in a userdata so it doesn't get copied when our instance does.
 	instance[name_type_checker] = newproxy(true)
-	getmetatable(instance[name_type_checker]).__index = self.typecheck
+	getmetatable(instance[name_type_checker]).__index = self.__typecheck
 
 	-- InstanceIndirection attribute wraps the object in a userdata
 	-- This allows a __gc metamethod with Lua 5.1 and LuaJIT.
@@ -181,8 +271,8 @@ OOP.Object[name_placement_constructor] = function(self, instance, ...)
 	instance = handle_indirection(self, instance)
 	apply_metatable(self, instance)
 
-	for attribute in pairs(self.attributes) do
-		local found = OOP.Attributes.PreConstructor[attribute]
+	for attribute in pairs(self.__attributes) do
+		local found = OOP.Attributes.PreInitialize[attribute]
 		if (found) then
 			found(self, instance)
 		end
@@ -193,8 +283,8 @@ OOP.Object[name_placement_constructor] = function(self, instance, ...)
 		instance[name_initializer](instance, ...)
 	end
 
-	for attribute in pairs(self.attributes) do
-		local found = OOP.Attributes.PostConstructor[attribute]
+	for attribute in pairs(self.__attributes) do
+		local found = OOP.Attributes.PostInitialize[attribute]
 		if (found) then
 			found(self, instance)
 		end
@@ -203,22 +293,40 @@ OOP.Object[name_placement_constructor] = function(self, instance, ...)
 	return instance
 end
 
--- Constructor wrapper
+--[[
+	[This method can be renamed, see Graphite/_.lua]
+	Object Class:New(...)
+
+	Creates a new object and passes parameters to its initializer.
+]]
 OOP.Object[name_constructor] = function(self, ...)
-	return self[name_placement_constructor](self, {}, ...)
+	return self[name_placement_constructor](self, nil, ...)
 end
 
-OOP.Object.members[name_copy] = function(self)
-	local copy = Dictionary.DeepCopy(self.self)
+--[[
+	[This method can be renamed, see Graphite/_.lua]
+	Object Object:Copy()
 
-	if (self.class.attributes.SparseInstances) then
-		setmetatable(copy, {__index = self.class.members})
+	Copies the given object.
+]]
+OOP.Object.__base_members[name_copy] = function(self)
+	local class = self.class
+	local target
+
+	if (class.__attributes.PooledInstantiation) then
+		target = table.remove(class.__pool, #class.__pool) or {}
 	end
 
-	copy = handle_indirection(self.class, copy)
-	apply_metatable(self.class, copy)
+	local copy = Dictionary.DeepCopy(self.self, target)
 
-	for attribute in pairs(self.class.attributes) do
+	if (class.__attributes.SparseInstances) then
+		setmetatable(copy, {__index = class.__members})
+	end
+
+	copy = handle_indirection(class, copy)
+	apply_metatable(class, copy)
+
+	for attribute in pairs(class.__attributes) do
 		local found = OOP.Attributes.Copy[attribute]
 		if (found) then
 			found(self, copy)
@@ -228,46 +336,51 @@ OOP.Object.members[name_copy] = function(self)
 	return copy
 end
 
+-- StaticObject, the base class for all singleton classes.
 OOP.StaticObject = Dictionary.DeepCopy(OOP.BaseClass)
-	--:Attributes(default_static_attributes)
+	:Attributes(default_static_attributes)
 
-OOP.StaticObject.members.class = newproxy(true)
-getmetatable(OOP.StaticObject.members.class).__index = OOP.StaticObject
+OOP.StaticObject.__members.class = newproxy(true)
+getmetatable(OOP.StaticObject.__members.class).__index = OOP.StaticObject
 
-OOP.StaticObject.typecheck[OOP.StaticObject] = true
-OOP.StaticObject[name_type_checker] = OOP.StaticObject.typecheck
+OOP.StaticObject.__typecheck[OOP.StaticObject] = true
+OOP.StaticObject[name_type_checker] = OOP.StaticObject.__typecheck
 
+--[[
+	Class OOP:Class()
+
+	Creates a new, empty class.
+]]
 function OOP:Class()
 	local class = Dictionary.DeepCopy(self.BaseClass)
 
 	Dictionary.DeepCopy(self.Object, class)
 
-	class.typecheck[class] = true
-
-	class.class = newproxy(true)
-	getmetatable(class.class).__index = class
+	class.__typecheck[class] = true
 
 	setmetatable(class, {
-		__newindex = class.members,
-		__index = class.members
+		__newindex = class.__members,
+		__index = class.__members
 	})
 
 	return class
 end
 
+--[[
+	StaticClass OOP:StaticClass()
+
+	Creates a static (singleton) class.
+]]
 function OOP:StaticClass()
 	local class = Dictionary.ShallowCopy(self.BaseClass)
 
 	Dictionary.DeepCopy(self.StaticObject, class)
 
-	class.typecheck[class] = true
-
-	class.class = newproxy(true)
-	getmetatable(class.class).__index = class
+	class.__typecheck[class] = true
 
 	setmetatable(class, {
-		__newindex = class.members,
-		__index = class.members
+		__newindex = class.__members,
+		__index = class.__members
 	})
 
 	return class
